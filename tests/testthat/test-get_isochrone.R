51612b4cb726e06d21ae926a58f70b76db85366b
Paul Hewson <texhewson@gmail.com>
2020-12-23 12:01:29 +0000

    Add tests for isochrones
    Remove "fixture", split into two tests. Test data frame output.
    
    Working on #16

--- tests/testthat/test-get_isochrone.R
+++ tests/testthat/test-get_isochrone.R
@@ -1,24 +1,33 @@
 context("Get isochrone")
 library(cppRouting)
 
-test_that("directed graph distance matrix", {
-## Given a simple graph      3
-##                        d3 d6 d13
-##                     1 d2 2  d6d  4  d22  5
-    edges <- data.frame(
-        from_vertex = c(0, 0, 1, 1, 2, 2, 3, 4, 4),
-        to_vertex = c(1, 3, 2, 4, 4, 5, 1, 3, 5),
-        cost = c(9, 2, 11, 3, 5, 12, 4, 1, 6))
-
+## A simple graph
+##         +------->-------3
+##         |             ^ |
+##         |           d2 d4 
+##         |          /    v
+##         ^        0-d3-->1-d11->2-d12>5
+##         |                \     |     |
+##         |                 d3   d5    ^
+##         |                  v  v      |
+##         d1 --------<------- 4 --->---d6
+edges <- data.frame(
+    from_vertex = c(0, 0, 1, 1, 2, 2, 3, 4, 4),
+    to_vertex =   c(1, 3, 2, 4, 4, 5, 1, 3, 5),
+    cost =        c(9, 2, 11, 3, 5, 12, 4, 1, 6))
 directed_graph <- makegraph(edges, directed = TRUE)
 
-#Get nodes reachable around node 4 with maximum distances of 1 and 2
-iso <- get_isochrone(Graph = directed_graph, from = "4", lim = c(1, 2))
+expected <- data.frame(origin = as.character(c(4, 4, 4)),
+                       node = as.character(c(4, 3, 4)),
+                       lim = as.character(c(1, 2, 2)))
 
-#With setdif set to TRUE
-iso2 <- get_isochrone(Graph = directed_graph, from = "4", lim = c(1, 2), setdif = TRUE)
-print(iso)
-print(iso2)
+test_that("isochrones by limit", {
+    iso <- get_isochrone(Graph = directed_graph, from = "4", lim = c(1, 2), long = TRUE)
+    expect_equal(iso, expected)
+})
 
-  expect_equal(iso[[1]][[1]], "4")
+test_that("isochrones by bucket", {
+    iso2 <- get_isochrone(Graph = directed_graph, from = "4", lim = c(1, 2),
+                          setdif = TRUE, long = TRUE)
+    expect_equal(iso2, expected[-3, ])
 })
